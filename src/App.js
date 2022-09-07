bb3981da1c5e330438f19afa2d7eb43208b1aba3
harimgs <harimgs@hotmail.com>
2022-09-06 23:19:22 -0400

    final final commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Home from "./routes/Home";
 function App() {
   return <Router>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path=`${process.env.PUBLIC_URL}/` element={<Home />} />
       <Route path="/movie/:id" element={<Detail />} />
     </Routes>
   </Router>;
